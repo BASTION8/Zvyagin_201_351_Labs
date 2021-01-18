65dde1fb67c5c0266149edc5679eda73dc603a3d
tolzvyagin@gmail.com <tolzvyagin@gmail.com>
2021-01-18 18:11:02 +0300

    добавление лабораторной 7

---
+++ Lab7_Templates/Lab7_Templates.cpp
@@ -0,0 +1,141 @@
+﻿#include <iostream>
+#include <string>
+#include <ctime>
+
+struct Student
+{
+	std::string lastName;
+    int age;
+
+    Student()
+    {
+        lastName = "";
+        age = 0;
+    }
+    Student(std::string _lastName, int _age)
+    {
+        lastName = _lastName;
+        age = _age;
+    }
+
+    bool operator > (Student p)
+    {
+        return age > p.age;
+    }
+    bool operator < (Student p)
+    {
+        return age < p.age;
+    }
+
+    friend std::ostream& operator << (std::ostream& os, Student& p);
+};
+
+std::ostream& operator << (std::ostream& os, Student& p)
+{
+	std::cout << p.lastName << ":" << p.age;
+
+    return std::cout;
+}
+
+template <typename T>
+void qSort(T* arr, int left, int right)
+{
+    int i = left;
+    int j = right;
+    T pivot = arr[(i + j) / 2];
+    T temp;
+
+    while (i <= j)
+    {
+        while (arr[i] < pivot) i++;
+        while (arr[j] > pivot) j--;
+
+        if (i <= j)
+        {
+            temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+            i++;
+            j--;
+        }
+    }
+
+    if (j > left) 
+        qSort(arr, left, j);
+    if (i < right)
+        qSort(arr, i, right);
+}
+
+template <typename T>
+void printArray(T* array, int n)
+{
+    for (int i = 0; i < n; i++) 
+        std::cout << array[i] << "  ";
+    std::cout << std::endl;
+}
+
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    srand(time(0));
+    int n;
+    std::cout << "Введите число:";
+    std::cin >> n;
+    bool* arr = new bool[n];
+    for (int i = 0; i < n; i++)
+        arr[i] = rand() % 2;
+
+    std::cout << "Перед Quick Sort [bool]:" << std::endl;
+    printArray(arr, n);
+
+    qSort(arr, 0, n - 1);
+
+    std::cout << "После Quick Sort [bool]:" << std::endl;
+    printArray(arr, n);
+    std::cout << "\n\n";
+
+    int* arr1 = new int[n];
+
+    for (int i = 0; i < n; i++)
+        arr1[i] = -50 + rand() % 100;
+
+    std::cout << "Перед Quick Sort [int]:" << std::endl;
+    printArray(arr1, n);
+
+    qSort(arr1, 0, n - 1);
+
+    std::cout << "После Quick Sort [int]:" << std::endl;
+    printArray(arr1, n);
+    std::cout << "\n\n";
+
+    double* arr2 = new double[n];
+
+    for (int i = 0; i < n; i++)
+        arr2[i] = -1 + (double)rand() / 10000;
+
+    std::cout << "Перед Quick Sort [double]:" << std::endl;
+    printArray(arr2, n);
+
+    qSort(arr2, 0, n - 1);
+
+    std::cout << "После Quick Sort [double]:" << std::endl;
+    printArray(arr2, n);
+    std::cout << "\n\n";
+
+    Student* p = new Student[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        p[i].lastName = "Имя(" + std::to_string(i) + ")";
+        p[i].age = 15 + rand() % 21;
+    }
+
+    std::cout << "Перед Quick Sort [Student]:" << std::endl;
+    printArray(p, n);
+
+    qSort(p, 0, n - 1);
+
+    std::cout << "После Quick Sort [Student]:" << std::endl;
+    printArray(p, n);
+}
--- Labs6_StructuresData/Labs6_StructuresData.cpp
+++ Labs6_StructuresData/Labs6_StructuresData.cpp
@@ -1,16 +1,18 @@
-﻿#include <string>
+﻿#include <ctime>
+#include <string>
 #include <stack>
 #include <iostream>
 #include <deque>
 #include <queue>
-#include <set>
 #include <list>
 #include <map>
+#include <unordered_set>
 
-
-int task1(std::string s)
+int task1()
 {
-
+	std::string s;
+	std::cout << "Введите строку:" << std::endl;
+	std::cin >> s;
 	std::stack<int> p;
 	if (s.size() == 1)
 	{
@@ -46,8 +48,11 @@ int task1(std::string s)
 	return 0;
 }
 
-void task2(std::string s)
+void task2()
 {
+	std::string s;
+	std::cout << "Введите строку:" << std::endl;
+	std::cin >> s;
 	std::deque<char> d;
 	for (int i = 0; i < s.size(); i++)
 	{
@@ -61,87 +66,201 @@ void task2(std::string s)
 	std::cout << (d.empty() || d.size() == 1 ? "Палиндром " : "Не палиндром ");
 }
 
-bool task3_if(int n)
+int min_task3(int a1, int a2, int a3)
 {
-	if (n % 2 == 0 || n % 3 == 0 || n % 5 == 0)
+	if ((a1 <= a2) and (a1 <= a3))
 	{
-		return true;
+		return a1;
 	}
-	else
-		return false;
+	if ((a2 <= a1) and (a2 <= a3))
+	{
+		return a2;
+	}
+	if ((a3 <= a2) and (a3 <= a1))
+	{
+		return a3;
+	}
+}
+
+void add_print_task3(int t, std::queue<int>& q2, std::queue<int>& q3, std::queue<int>& q5)
+{
+	if (t != 1)
+	{
+		std::cout << t << ' ';
+	}
+	q2.push(t * 2); 
+	q3.push(t * 3); 
+	q5.push(t * 5); 
 }
 
 void task3()
 {
-	int size1;
-	std::cin >> size1;
-	std::queue<int>q1;
-	for (int i = 1; i < size1; i++)
+	std::cout << "Введите число:\n";
+	int n;
+	std::cin >> n;
+	std::queue<int> q2,q3,q5;
+	q2.push(2);
+	q3.push(3);
+	q5.push(5);
+	int k = 0;
+	while (k != n)
 	{
-		if (task3_if(i))
-			q1.push(i);
-	}
-	while (!q1.empty())
-	{
-		std::cout << q1.front() << std::endl;
-		q1.pop();
+		int x = min_task3(q2.front(), q3.front(), q5.front());
+		add_print_task3(x, q2, q3, q5);
+		k++;
+		if (x == q2.front())
+			q2.pop();
+		if (x == q3.front())
+			q3.pop();
+		if (x == q5.front())
+			q5.pop();
 	}
 }
 
-void task4(std::string s)
+class student1
 {
-	int size1;
-	std::cin >> size1;
-	std::queue<int>q1;
-	for (int i = 1; i < size1; i++)
-	{
-		if (task3_if(i))
-			q1.push(i);
-	}
-	while (!q1.empty())
-	{
-		std::cout << q1.front() << std::endl;
-		q1.pop();
-	}
-}
+public:
 
-void task5(std::string s)
+	student1(std::string s, int m)
+	{
+		account = s;
+		mark = m;
+	}
+
+	std::string account;
+	int mark;
+
+	friend std::ostream& operator << (std::ostream& os, student1 p)
+	{
+		os << p.account << ": " << p.mark;
+
+		return os;
+	}
+
+	bool operator == (const student1& p) const
+	{
+		return p.account == account;
+	}
+};
+
+// структура для хэша
+struct hash
 {
-	int size1;
-	std::cin >> size1;
-	std::queue<int>q1;
-	for (int i = 1; i < size1; i++)
+	size_t operator()(const student1& v) const
 	{
-		if (task3_if(i))
-			q1.push(i);
+		return std::hash<std::string>{}(v.account);
 	}
-	while (!q1.empty())
-	{
-		std::cout << q1.front() << std::endl;
-		q1.pop();
-	}
-}
+};
 
-void task6(std::string s)
+void task4()
 {
-	int size1;
-	std::cin >> size1;
-	std::queue<int>q1;
-	for (int i = 1; i < size1; i++)
+	std::unordered_set<student1, hash> pupils;
+
+	srand(time(0));
+
+	int n = 10;
+
+	std::cout << "Все попытки:\n\n";
+	for (int i = 0; i < n; i++)
 	{
-		if (task3_if(i))
-			q1.push(i);
+		student1 p("аккаунт " + std::to_string(1 + rand() % 10), rand() % 101);
+
+		std::cout << p << std::endl;
+
+		pupils.insert(p);
 	}
-	while (!q1.empty())
+	std::cout << "\nПринятые запросы:\n";
+	for (auto& i : pupils) std::cout << i << std::endl;
+}
+
+void task5()
+{
+	std::map<std::string, int> all_words; 
+
+	char text[1000];
+	getchar();
+	std::cin.getline(text, sizeof(text)); 
+	char s[] = " ,\t\n"; 
+	char* remained_text = NULL; 
+	char* word = strtok_s(text, s, &remained_text); 
+
+	int words_count = 0; 
+	while (word != NULL) 
 	{
-		std::cout << q1.front() << std::endl;
-		q1.pop();
+		all_words[word]++;
+		words_count++;
+
+		word = strtok_s(NULL, s, &remained_text);
+	}
+	std::cout << "\n\n";
+
+	for (auto& word : all_words)
+	{
+		std::cout << word.second << " " << word.first << " (" << (double)word.second / words_count * 100 << "%)" << std::endl;
 	}
 }
 
-int main() {
+class student
+{
+public:
 
-	std::string s;
+	student(std::string gr, std::string ln)
+	{
+		group = gr;
+		last_name = ln;
+	}
+
+	std::string group;
+	std::string last_name;
+
+	friend std::ostream& operator << (std::ostream& os, student& st)
+	{
+		os << st.group << " " << st.last_name;
+
+		return os;
+	}
+};
+
+void task6()
+{
+	int n = 20;
+	srand(time(0));
+	std::list<student> students;
+	std::vector<std::string> keys;
+
+	std::string names[] = { "Вася", "Коля", "Максим", "Артём", "Илья", "Лена" };
+	int length = sizeof(names) / sizeof(names[0]);
+
+	for (int i = 0; i < n; i++)
+	{
+		students.push_back(student("201-30" + std::to_string(rand() % 5), names[rand() % length] + "(" + std::to_string(i + 1) + ")"));
+
+		std::cout << students.back() << std::endl;
+	}
+	std::cout << "\n\n";
+	for (auto& i : students)
+	{
+		std::string key = i.group;
+		bool b = false; 
+
+		for (auto& st : students)
+		{
+
+			if (find(keys.begin(), keys.end(), key) == keys.end() && st.group == key)
+			{
+				std::cout << st << std::endl;
+				b = true;
+			}
+		}
+		keys.push_back(key);
+
+		if (b)
+			std::cout << std::endl;
+	}
+}
+
+int main()
+{	
 	setlocale(LC_ALL, "Russian");
 	int choice = 0;
 	while (true)
@@ -154,17 +273,16 @@ int main() {
 			<< "5. \n"
 			<< "6. \n";
 		std::cin >> choice;
-		std::cin >> s;
 		switch (choice)
 		{
 		case 1:
 		{
-			task1(s);
+			task1();
 			break;
 		}
 		case 2:
 		{
-			task2(s);
+			task2();
 			break;
 		}
 		case 3:
@@ -174,17 +292,17 @@ int main() {
 		}
 		case 4:
 		{
-			task4(s);
+			task4();
 			break;
 		}
 		case 5:
 		{
-			task5(s);
+			task5();
 			break;
 		}
 		case 6:
 		{
-			task6(s);
+			task6();
 			break;
 		}
 		default:
